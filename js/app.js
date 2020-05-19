cc1a295fbe29b04271a690f9abcaf85a913c64aa
Iván Giovanazzi <ivanog@pm.me>
2020-05-19 01:07:02 -0300

    added-eventMath-function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var keyValue,
-   cal = '';
+   cal = ''
 
 var displayMath = function (cal) {
    var el = document.getElementById('screen')
@@ -15,3 +15,38 @@ var displayWarning = function (exception) {
    el.style.color = '#ff1515';
    console.log('Exception thrown: ' + exception)
 }
+
+var eventsMath = function (e) {
+   keyValue = e.target.textContent
+   if (keyValue === 'Limpiar') {
+      cal = ''
+      displayMath(cal)
+   } else if (keyValue === 'Borrar') {
+      cal = cal.slice(0, -1)
+      displayMath(cal)
+   } else if (keyValue === '=') {
+      try {
+         cal = eval(cal)
+         displayMath(cal)
+         cal = ''
+      } catch (ex) {
+         displayWarning(ex)
+      }
+   } else {
+      cal += keyValue
+      displayMath(cal)
+   }
+}
+
+const items = document.getElementsByClassName('item')
+
+console.log(items.length)
+
+
+// for (var i = 0; i < items.length; i++) {
+//    console.log(i)
+//    // items[i].addEventListener('click', eventsMath)
+// }
+// items.forEach(elem => {
+//    elem.addEventListener('click', eventsMath)
+// });
